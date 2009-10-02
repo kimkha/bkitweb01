1ba3d3e053645d04accc93e3fdceee9b3c39ef37
hungvinhbk <hungvinhbk@934f673a-9d2b-11de-82a1-83fd6072038f>
2009-10-02 16:50:06 +0000

    chưa xong

--- trunk/src/views/event.php
+++ trunk/src/views/event.php
@@ -1,10 +1,58 @@
 <?php
-
-	$html = <<<EOT
-
-This is the view of BKITEvent
-	
+	/**
+	 * Get Event and return a html string
+	 * 
+	 * @param $viewname array('object'=> $object 
+	 * 						 'viewtype' => $viewtype)
+	 * @return $output : html string
+	 */
+	 $obj  = $vars['object'];
+	 $type = $vars['viewtype'];
+	 
+	 $output = '';
+	 switch($type){
+	 	case 'short':
+			$output .= <<<EOT
+			<div class="post">
+			<h2><a href="event.php?id=$obj->get('eid')"><B>[$obj->get('name')]</B> $obj->get('title')</a></h2>
+				
+			<p class="post-info">Posted by <a href="">$obj->get('name')</a></p>
+			
+			<p>$obj->get('headline')</p>
+						
+					
+			<p class="postmeta">		
+			<a href="event.php?id=$obj->get('eid')" class="readmore">Read more</a> |				
+			<span class="date">$obj->get('time_updated')</span>	
+			</p>
+			</div>
 EOT;
+			break;
+		case 'full':
+			$output .= <<<EOT
+			<div class="post">
+			<h2><a href="event.php?id=$obj->get('eid')"><B>[$obj->get('name')]</B> $obj->get('title')</a></h2>
+				
+			<p class="post-info">Posted by <a href="">$obj->get('name')</a></p>
+			<p><b>$obj->get('headline')<b/></p>
+			<p>$obj->get('content')</p>
+						
+					
+			<p class="postmeta">						
+			<span class="date">$obj->get('time_updated')</span>	
+			</p>
+			</div>			
+EOT;
+			break;	
+		case 'title':
+			$output.= <<<EOT
+			<li><a href="event.php?id=$obj->get('eid')"><B>[$obj->get('name')]</B> $obj->get('title')</a></li>
+EOT;
+			break;
+ 	
+	 }
+ 
+
 	
-	return $html;
+	return $output;
 ?>
