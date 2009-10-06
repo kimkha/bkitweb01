bfd3b8415b5d9ef85b74b24bc1be41dfe8931e6d
hungvinhbk <hungvinhbk@934f673a-9d2b-11de-82a1-83fd6072038f>
2009-10-06 08:02:55 +0000

    Update Layout !!!

--- trunk/src/themes/default/footer.php
+++ trunk/src/themes/default/footer.php
@@ -6,17 +6,13 @@
 		<div class="bottom-left">
 			<p>
 			&copy; 2009 <strong>BkitWeb Project</strong>&nbsp; &nbsp; &nbsp;
-			Design by: <a href="http://www.bkitclub.net/">BkitWeb</a> 			
+						
 			</p>
 		</div>
 	
 		<div class="bottom-right">
 			<p>		
-				<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | 
-		   	<a href="http://validator.w3.org/check/referer">XHTML</a>	|			
-				<a href="index.html">Home</a> |
-				<a href="index.html">Sitemap</a> |
-				<a href="index.html">RSS Feed</a>								
+				<a href="http://www.bkitclub.net/">BkitWeb @</a> 								
 			</p>
 		</div>
 	<!-- footer-bottom ends -->		
